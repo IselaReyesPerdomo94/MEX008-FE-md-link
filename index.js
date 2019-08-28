89f32cd0a5048fa75f9e8ad5958916dba6a8dd86
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-08-27 23:50:17 -0500

    Agregando requerimientos para manejo de archivos MD, utilizando modulo path, fs y markdownIt

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+const fs = require('fs');
+const markdownIt = require('markdown-it');
+const md = new markdownIt();
+
+module.exports = (path, options) => {
+    console.log(path.win32.basename(path));
+    console.log('si funciona')
+
+    //should identify if path is a file or a dir. maybe i will use file.readFile || file.readdir
+    fs.readFile(path, (err, data) => {
+        if (err) throw err;
+        console.log(data)
+    })
+
+    //should identify wich file is a markdown file. Maybe will use markdownIt || 
+
+    //should return a promise that resolves an array of objects
+    //return 
 };
